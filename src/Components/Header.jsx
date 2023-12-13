d06f2191503bd1877198dfef1b5da52de40abb61
Onkar Karale <onkarkarale4@gmail.com>
2023-12-13 15:41:22 +0530

    Modified Header and Sidebar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   }
 
   return (
-    <div className='flex justify-between items-center px-3 py-2'>
+    <div className='flex justify-between items-center px-3 py-3'>
       <div className='flex'>
         <div className='p-1 hover:bg-gray-200 hover:rounded-full'>
           <FiMenu className='w-10 h-6 cursor-pointer' onClick={() => toggleMenuHandler()} />
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import { Explore } from '../utils/constants';
 import { Premium } from '../utils/constants';
 import { Setting } from '../utils/constants';
-import { Link } from 'react-router-dom'
 import { IoMdHome } from "react-icons/io";
 import { FaSquareYoutube } from "react-icons/fa6";
 import { SiYoutubeshorts } from "react-icons/si";
@@ -39,87 +38,89 @@ const SideBar = () => {
       </div>
     </div>
     : (
-      <div className='sticky shadow-2xl'>
-        <ul className='ml-3 mt-2'>
-          <li className='flex items-center gap-5 bg-gray-100 w-52 pl-3 rounded-lg cursor-pointer'>
-            <IoMdHome className='w-6 h-10' />
-            <span className='font-bold'>Home</span>
-          </li>
-          <li className='flex items-center gap-5 pl-3 hover:bg-gray-100 hover:rounded-lg hover:w-52 cursor-pointer'>
-            <SiYoutubeshorts className='w-6 h-10' />
-            Shorts
-          </li>
-          <li className='flex items-center gap-5 pl-3 hover:bg-gray-100 hover:rounded-lg hover:w-52  cursor-pointer'>
-            <MdSubscriptions className='w-6 h-10' />
-            Subscriptions
-          </li>
-        </ul>
-        <div className='mt-3 pl-3 w-70'>
-          <hr />
-        </div>
-
-        <div className='ml-3 my-4'>
-          <h1 className='font-bold ml-3 mb-2'>Explore</h1>
-          <ul>
-            {
-              Explore.map(({ icon, name }) => {
-                return (
-                  <a href='#' key={name}>
-                    <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
-                      {icon}
-                      <span>{name}</span>
-                    </li>
-                  </a>
-                )
-              })
-            }
+      <>
+        <div className='sticky shadow-2xl overflow-y-auto h-screen'>
+          <ul className='ml-3 mt-2'>
+            <li className='flex items-center gap-5 bg-gray-100 w-52 pl-3 rounded-lg cursor-pointer'>
+              <IoMdHome className='w-6 h-10' />
+              <span className='font-bold'>Home</span>
+            </li>
+            <li className='flex items-center gap-5 pl-3 hover:bg-gray-100 hover:rounded-lg hover:w-52 cursor-pointer'>
+              <SiYoutubeshorts className='w-6 h-10' />
+              Shorts
+            </li>
+            <li className='flex items-center gap-5 pl-3 hover:bg-gray-100 hover:rounded-lg hover:w-52  cursor-pointer'>
+              <MdSubscriptions className='w-6 h-10' />
+              Subscriptions
+            </li>
           </ul>
-        </div>
+          <div className='mt-3 pl-3 w-60'>
+            <hr />
+          </div>
 
-        <div className='mt-3 pl-3 w-70'>
-          <hr />
-        </div>
+          <div className='ml-3 my-4'>
+            <h1 className='font-bold ml-3 mb-2'>Explore</h1>
+            <ul>
+              {
+                Explore.map(({ icon, name }) => {
+                  return (
+                    <a href='#' key={name}>
+                      <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
+                        {icon}
+                        <span>{name}</span>
+                      </li>
+                    </a>
+                  )
+                })
+              }
+            </ul>
+          </div>
 
-        <div className='ml-3 my-4'>
-          <h1 className='font-bold ml-3 mb-2'>More from YouTube</h1>
-          <ul>
-            {
-              Premium.map(({ icon, name }) => {
-                return (
-                  <a href='#' key={name}>
-                    <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
-                      {icon}
-                      <span>{name}</span>
-                    </li>
-                  </a>
-                )
-              })
-            }
-          </ul>
-        </div>
+          <div className='mt-3 pl-3 w-60'>
+            <hr />
+          </div>
 
-        <div className='mt-3 pl-3 w-70'>
-          <hr />
-        </div>
+          <div className='ml-3 my-4'>
+            <h1 className='font-bold ml-3 mb-2'>More from YouTube</h1>
+            <ul>
+              {
+                Premium.map(({ icon, name }) => {
+                  return (
+                    <a href='#' key={name}>
+                      <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
+                        {icon}
+                        <span>{name}</span>
+                      </li>
+                    </a>
+                  )
+                })
+              }
+            </ul>
+          </div>
 
-        <div className='ml-3 my-4'>
-          <ul>
-            {
-              Setting.map(({ icon, name }) => {
-                return (
-                  <a href='#' key={name}>
-                    <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
-                      {icon}
-                      <span>{name}</span>
-                    </li>
-                  </a>
-                )
-              })
-            }
-          </ul>
-        </div>
+          <div className='mt-3 pl-3 w-60'>
+            <hr />
+          </div>
 
-      </div>
+          <div className='ml-3 my-4'>
+            <ul>
+              {
+                Setting.map(({ icon, name }) => {
+                  return (
+                    <a href='#' key={name}>
+                      <li className='flex items-center gap-5 pl-3 w-60 rounded-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg hover:w-52 '>
+                        {icon}
+                        <span>{name}</span>
+                      </li>
+                    </a>
+                  )
+                })
+              }
+            </ul>
+          </div>
+
+        </div>
+      </>
     )
 }
 
