e458eb502ebac6a23ae639f3a4868631997007dc
Onkar Karale <onkarkarale4@gmail.com>
2023-12-30 20:28:58 +0530

    Updated responsive Sidebar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -38,8 +38,7 @@ const SideBar = () => {
     </div>
     : (
       <>
-        <div className='fixed shadow-2xl hover:overflow-y-scroll w-[19vw] h-full bg-white z-10 text-sm hover:overflow-x-hidden top-[55px] transition-shadow duration-300'>
-
+        <div className='fixed shadow-2xl hover:overflow-y-scroll md:w-[32vw] lg:w-[19vw] h-full bg-white z-10 text-sm hover:overflow-x-hidden top-[55px] transition-shadow duration-300'>
           <ul className='ml-4 mt-3'>
             <li className=' bg-gray-100 w-52 pl-[13px] rounded-lg'>
               <NavLink to='/' className='flex items-center gap-5'>
