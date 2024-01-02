8b7f55f36ecbed0b7913560055210e683d4d17e4
Onkar Karale <onkarkarale4@gmail.com>
2024-01-02 20:37:18 +0530

    Added nad modified responsive searchBar in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,47 +1,88 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
-import { FaUserCircle } from 'react-icons/fa';
-import { IoSearchOutline } from 'react-icons/io5';
-import { IoNotificationsOutline } from 'react-icons/io5';
+import { IoSearchOutline, IoNotificationsOutline } from 'react-icons/io5';
 import { RiVideoAddLine } from 'react-icons/ri';
+import { BsArrowLeftShort } from "react-icons/bs";
 import YouTubeLogo from '../assets/YouTube_Logo.png';
 import MyPic from '../assets/MyPic.jpg';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
 
+
+
 const Header = () => {
   const dispatch = useDispatch();
+  const [showSearch, setShowSearch] = useState(false);
 
+  // Function to toggle the side menu
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
+  };
+
+  // Function to handle the search input focus
+  const handleSearchFocus = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 768 && !showSearch) {
+      setShowSearch(true);
+    }
+  };
+
+  // Function to handle the arrow left button click in the search
+  const handleArrowLeftClick = () => {
+    setShowSearch(false);
+  };
+
+  // Function to handle the search button click
+  const handleSearchButtonClick = () => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 768 && !showSearch) {
+      setShowSearch(true);
+    }
   };
 
   return (
     <div className='flex justify-between items-center md:px-4 px-2 pr-4 py-2 bg-white fixed w-full z-20'>
 
-      <div className='flex items-center'>
+      {/* Arrow Left In sm Search To Move to normal Screen */}
+      {
+        showSearch &&
+        <div className='flex items-center mx-1'>
+          <button onClick={handleArrowLeftClick}>
+            <BsArrowLeftShort className='text-4xl' />
+          </button>
+        </div>
+      }
+
+      {/* Logo and Menu Icon */}
+      <div className={`${showSearch ? 'max-sm:hidden' : ''} flex items-center`}>
         <div className='p-1 hover:bg-gray-200 rounded-full'>
-          <FiMenu className='w-10 h-6 cursor-pointer' onClick={() => toggleMenuHandler()} />
+          <FiMenu className='w-10 h-6 cursor-pointer' onClick={toggleMenuHandler} />
         </div>
         <img src={YouTubeLogo} alt="YouTubeLogo" className='w-22 h-8 cursor-pointer' />
       </div>
 
-      <div className='hidden md:flex w-1/2'>
+      {/* Search Input */}
+      <div className='flex max-sm:mr-5'>
         <input
           type="text"
           placeholder='Search'
-          className='rounded-s-full w-11/12 h-10 px-5 border border-gray-400 items-center'
+          className={`${showSearch ? 'w-[60vw]' : 'max-sm:hidden'} md:w-[40vw] border border-gray-400 rounded-l-full py-1 pl-3 md:pl-5`}
+          onFocus={handleSearchFocus}
         />
-        <button className='border border-gray-400 rounded-r-full px-2 bg-neutral-50 hover:bg-gray-100'>
-          <IoSearchOutline className='w-10 h-6' />
+        <button
+          className={`border border-gray-400 ${!showSearch ? 'max-sm:border-none max-sm:rounded-full max-sm:text-2xl max-sm:ml-10' : ''} rounded-r-full md:py-2 px-2 md:px-5 flex justify-center items-center  hover:bg-gray-100`}
+          onClick={handleSearchButtonClick}
+        >
+          <IoSearchOutline className='md:w-10 md:h-6' />
         </button>
       </div>
 
+      {/* Notification and User Icons */}
       <div className='flex items-center gap-8 text-2xl md:mr-3'>
         <RiVideoAddLine className='hover:cursor-pointer md:flex hidden' />
-        <div className='relative'>
+        <div className={`${showSearch ? 'max-sm:hidden' : ''} relative`}>
           <IoNotificationsOutline className='hover:cursor-pointer relative' />
           <span className='absolute bottom-3 left-3 text-xs bg-red-600 rounded-full px-1 text-white hover:cursor-pointer'>
             9+
