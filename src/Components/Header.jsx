0ca474fa2812ac7c64867e287688871ab484b568
Onkar Karale <onkarkarale4@gmail.com>
2024-01-03 10:31:12 +0530

    Modified Search Button for Small Screen Devices

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -68,10 +68,10 @@ const Header = () => {
         <input
           type="text"
           placeholder='Search'
-          className={`${showSearch ? 'w-[60vw] focus:outline-blue-800' : 'max-sm:hidden'} md:w-[40vw] border border-gray-400 rounded-l-full py-1 pl-3 md:pl-5`}
+          className={`${showSearch ? 'w-[60vw] focus:outline-red-600' : 'max-sm:hidden'} md:w-[40vw] border border-gray-400 rounded-l-full py-1 pl-3 md:pl-5`}
         />
         <button
-          className={`border border-gray-400 ${!showSearch ? 'max-sm:border-none max-sm:rounded-full max-sm:text-2xl max-sm:ml-10' : ''} rounded-r-full md:py-2 px-2 md:px-4 flex justify-center items-center  hover:bg-gray-100`}
+          className={`border border-gray-400 ${!showSearch ? 'max-sm:border-none max-sm:rounded-full max-sm:text-2xl max-sm:ml-20 ' : ''} rounded-r-full md:py-2 px-2 md:px-4 flex justify-center items-center  hover:bg-gray-100`}
           onClick={handleSearchButtonClick}
         >
           <IoSearchOutline className='md:w-10 h-6' />
