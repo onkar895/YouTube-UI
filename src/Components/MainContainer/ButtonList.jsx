6c3fa16746d69653c910746a7469250d7bbee600
Onkar Karale <onkarkarale4@gmail.com>
2024-01-05 11:59:44 +0530

    Modified ButtonList

--- src/Components/MainContainer/ButtonList.jsx
+++ src/Components/MainContainer/ButtonList.jsx
@@ -10,7 +10,7 @@ const ButtonList = () => {
       <ul className='flex text-sm gap-4 overflow-y-auto scrollBar whitespace-nowrap cursor-pointer'>
         {
           ButtonNames.map((name, index) => (
-            <li key={index} className={`bg-gray-100 hover:bg-gray-900 hover:text-white px-[12px] py-[6px] rounded-lg ${name === "All" ? "bg-gray-900 text-white" : " "}`}>
+            <li key={index} className={`bg-gray-100 hover:bg-gray-900 hover:text-white hover:transition duration-500 px-[12px] py-[6px] rounded-lg ${name === "All" ? "bg-gray-900 text-white" : " "}`}>
               <NavLink>
                 <span>
                   {name}
