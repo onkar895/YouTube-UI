c4d5bba98b32fbd303599b30a322e14438595438
Onkar Karale <onkarkarale4@gmail.com>
2024-01-08 23:33:07 +0530

    Updated VideoContainer

--- src/Components/MainContainer/VideoContainer.jsx
+++ src/Components/MainContainer/VideoContainer.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEO_API } from '../../utils/APIList';
 
 const VideoContainer = () => {
+
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
     getVideos();
@@ -11,8 +13,16 @@ const VideoContainer = () => {
   const getVideos = async () => {
     try {
       const response = await fetch(YOUTUBE_VIDEO_API);
-      const data = await response.json()
-      console.log(data);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch videos. Status: ${response.status}`);
+      }
+      // If the response is successful, it parses the JSON content of the response and updates the videos state with the received data. 
+      const data = await response.json();
+      // console.log(data.items);
+      setVideos(data.items)
+      // When you call setVideos(data.items), React will re-render the component with the new value of videos.
+      // That means, when the state of a component changes (in this case, when setVideos(data.items) is called), React uses a process called reconciliation to efficiently update the user interface.
     } catch (error) {
       console.error('Error while fetching the videos:', error);
     }
