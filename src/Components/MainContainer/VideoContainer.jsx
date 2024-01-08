0b73fd432ef51e53ccb868e3540d771e3d4159ad
Onkar Karale <onkarkarale4@gmail.com>
2024-01-08 21:33:12 +0530

    Modified VideoContainer and APILIST

--- src/Components/MainContainer/VideoContainer.jsx
+++ src/Components/MainContainer/VideoContainer.jsx
@@ -1,7 +1,24 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YOUTUBE_VIDEO_API } from '../../utils/APIList';
 
 const VideoContainer = () => {
+
+  useEffect(() => {
+    getVideos();
+  }, [])
+
+  const getVideos = async () => {
+    try {
+      const response = await fetch(YOUTUBE_VIDEO_API);
+      const data = await response.json()
+      console.log(data);
+    } catch (error) {
+      console.error('Error while fetching the videos:', error);
+    }
+  };
+
+
   return (
     <div>
       <h1>VideoContainer</h1>
--- src/utils/APIList.jsx
+++ src/utils/APIList.jsx
@@ -1,4 +1,8 @@
-/* eslint-disable no-undef */
-const API_KEY = process.env.GOOGLE_API_KEY;
+const API_KEY = import.meta.env.GOOGLE_API_KEY;
+// Vite uses the 'import.meta.env' object instead of 'process.env' to access environment variables.
+
+if (!API_KEY) {
+  console.error("API key is missing. Make sure you have set up the environment variable.");
+}
 
 export const YOUTUBE_VIDEO_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" + API_KEY;
