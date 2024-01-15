08ea29e20fb7177e2cc0743cf240114786dc8b44
Onkar Karale <onkarkarale4@gmail.com>
2024-01-15 12:52:44 +0530

    Added some styles in VideoShimmer

--- src/Components/ShimmerUI/VideoShimmer.jsx
+++ src/Components/ShimmerUI/VideoShimmer.jsx
@@ -10,18 +10,18 @@ const VideoShimmer = () => {
           .fill("")
           .map((_, index) => (
             <div key={index}>
-              <div className='mt-8 cursor-pointer md:w-[40.4vw] lg:w-[28.5vw] max-sm:w-[100vw] animation:spin 2s linear infinite'>
+              <div className='mt-8 cursor-pointer md:w-[40.4vw] lg:w-[28.5vw] max-sm:w-[100vw] animate-pulse'>
                 <div className='bg-gray-200 rounded-2xl max-sm:w-[90vw] md:h-[25vw] lg:h-[16vw] max-sm:h-[50vw] lg:w-[28.5vw] md:w-[40.4vw]'>
                 </div>
               </div>
-              <div className='flex max-sm:ml-6 mt-4 gap-2 items-center font-bold text-[14.6px]'>
+              <div className='flex max-sm:ml-6 mt-4 gap-2 items-center font-bold text-[14.6px] animate-pulse'>
                 <div className='rounded-full w-10 h-10 bg-gray-200' ></div>
                 <div className='mr-2 bg-gray-200 h-12 lg:w-[24.5vw] md:w-[33vw] max-sm:w-[75vw] rounded-lg'></div>
               </div>
-              <div className=' bg-gray-200 md:ml-12 max-sm:ml-[19vw] lg:w-[24.5vw] md:w-[33vw] max-sm:w-[75vw] h-4 mt-2 rounded-lg'>
+              <div className=' bg-gray-200 md:ml-12 max-sm:ml-[19vw] lg:w-[24.5vw] md:w-[33vw] max-sm:w-[75vw] h-4 mt-2 rounded-lg animate-pulse'>
 
               </div>
-              <div className=' bg-gray-200 md:ml-12 max-sm:ml-[19vw]  lg:w-[24.5vw] md:w-[33vw] max-sm:w-[75vw] h-4 mt-2 rounded-lg'>
+              <div className=' bg-gray-200 md:ml-12 max-sm:ml-[19vw]  lg:w-[24.5vw] md:w-[33vw] max-sm:w-[75vw] h-4 mt-2 rounded-lg animate-pulse'>
 
               </div>
             </div>
