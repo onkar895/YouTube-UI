ee0407e8b218b3d9d6ad6c01794fc4534bd88f1c
Onkar Karale <onkarkarale4@gmail.com>
2024-01-23 05:21:52 +0530

    Updated Header and SearchBar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -41,7 +41,7 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
     }
   };
 
-  const handleSearchSuggestionBar = `${showSearch ? "max-sm:w-[98%]" : ""}  fixed mt-10 py-5 bg-white shadow-2xl rounded-2xl md:w-[44vw] lg:w-[42.3vw] lg:h-[75vh] border border-gray-100`
+  const handleSearchSuggestionBar = `${showSearch ? "max-sm:w-[98%]" : "max-sm:hidden"}  fixed mt-10 py-5 bg-white shadow-2xl rounded-2xl md:w-[44vw] lg:w-[42.3vw] lg:h-[75vh] border border-gray-100`
 
   const handleInput = `${showSearch ? 'w-[62.5vw] mx-auto py-[8px] border-none bg-gray-200 transition-all duration-500 ml-2' : 'max-sm:hidden'} md:w-[36vw] lg:w-[42vw] md:py-[7px] lg:py-[7px] border border-gray-400 rounded-l-full py-1 pl-3 md:pl-6 focus:outline-gray-300 transition-all duration-500 ${isInputFocused ? 'max-sm:w-[75.5vw] max-sm:mx-auto' : ''}`
 
@@ -81,9 +81,13 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
           className={handleInput}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
-          onFocus={() => setShowSuggestions(true)}
+          onFocus={() => {
+            setIsInputFocused(true)
+            setShowSuggestions(true)
+          }
+          }
           onBlur={() => {
-            // Delay hiding suggestions to allow time for click events
+            setIsInputFocused(false);
             setTimeout(() => setShowSuggestions(false), 200);
           }}
         />
@@ -95,7 +99,7 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
         </button>
       </div>
       {
-        showSuggestions && (
+        searchQuery && showSuggestions && (
           <div className={handleSearchSuggestionBar}>
             <ul className='space-y-2 font-bold'>
               {
