b0dfda150804a19326ab8f2760553426b5e020fd
Onkar Karale <onkarkarale4@gmail.com>
2024-01-24 13:33:02 +0530

    Updated searchBar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -4,6 +4,7 @@
 import React, { useEffect, useState } from 'react';
 import { IoSearchOutline } from 'react-icons/io5';
 import { BsArrowLeftShort } from 'react-icons/bs';
+import { IoCloseOutline } from "react-icons/io5";
 import { YOUTUBE_SEARCH_API } from '../utils/APIList';
 import { useSelector } from 'react-redux';
 
@@ -33,9 +34,9 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
   // Styles stored in variables for better readability and maintainability
   const searchSuggestionBarStyles = `${showSearch ? 'max-sm:w-[98%] max-sm:rounded-b-2xl' : 'max-sm:hidden'} fixed py-5 bg-white md:shadow-2xl md:rounded-2xl md:w-[44.5vw] lg:w-[42.3vw] lg:h-[75vh] border border-gray-100 ${isMenuOpen ? 'md:ml-20 md:w-[38vw] lg:ml-0' : ''}`;
 
-  const inputStyles = `${showSearch ? 'w-[62.5vw] mx-auto transition-all duration-500 ml-2 pl-4 py-2' : 'max-sm:hidden'} md:w-[36vw] lg:w-[42vw] md:py-[7px] lg:py-[7px] border border-gray-300 rounded-l-xl py-1 pl-3 md:pl-6 transition-all focus:outline-0 duration-500 ${isInputFocused ? 'max-sm:w-[75.5vw] max-sm:mx-auto max-sm:focus:outline-0 md:pl-[3.2rem] md:border md:border-blue-700 max-sm:border max-sm:border-blue-700 ' : ''} ${isMenuOpen ? 'md:w-[29vw] md:ml-20 lg:ml-0' : ''}`;
+  const inputStyles = `${showSearch ? 'w-[62.5vw] mx-auto transition-all duration-500 ml-2 pl-4 py-2' : 'max-sm:hidden'} md:w-[36vw] lg:w-[42vw] md:py-[7px] lg:py-[7px] border border-gray-300 md:rounded-l-full max-sm:rounded-l-xl py-1 pl-3 md:pl-6 transition-all focus:outline-0 duration-500 ${isInputFocused ? 'max-sm:w-[75.5vw] max-sm:mx-auto max-sm:focus:outline-0 md:pl-[3.2rem] md:border md:border-blue-500 max-sm:border max-sm:border-blue-500 ' : ''} ${isMenuOpen ? 'md:w-[28.8vw] md:ml-20 lg:ml-0' : ''}`;
 
-  const searchButtonStyles = `${showSearch ? 'px-3 text-lg' : 'max-sm:border-none max-sm:text-2xl max-sm:ml-[7.5rem]'} border border-gray-300 hover:bg-gray-200 rounded-r-xl md:px-3 flex justify-center items-center md:bg-gray-100 ${isInputFocused ? 'md:border md:border-blue-700 max-sm:border max-sm:border-blue-700 ' : ''}`;
+  const searchButtonStyles = `${showSearch ? 'max-sm:px-3 max-sm:py-[10px] max-sm:rounded-r-xl' : 'max-sm:border-none max-sm:text-2xl max-sm:px-auto max-sm:pl-32'} text-xl px-[2px] py-[9px] border border-gray-300 hover:bg-gray-200 rounded-r-full md:px-6 flex justify-center items-center md:bg-gray-100 ${isInputFocused ? 'md:border md:border-blue-500 max-sm:border max-sm:border-blue-500 ' : ''}`;
 
   // Function to handle the search button click
   const handleSearchButtonClick = () => {
@@ -62,10 +63,14 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
     setTimeout(() => setShowSuggestions(false), 200);
   };
 
+  const handleClearSearch = () => {
+    setSearchQuery('');
+  };
+
   return (
     <>
       <div>
-        <div className='flex'>
+        <div className='flex items-center'>
           {/* Left Arrow Button In sm Search To Move to normal Screen */}
           {
             showSearch && (
@@ -79,9 +84,16 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
 
           {
             isInputFocused && (
-              <div className={`${isMenuOpen ? 'md:ml-20 lg:ml-0' : ''} absolute max-sm:hidden px-[11.5px] py-[10px]`}>
-                <IoSearchOutline className='md:w-10 md:h-5' />
-              </div>
+              <>
+                <div className={` absolute z-30 max-sm:hidden flex items-center`}>
+                  <div className={`${isMenuOpen ? 'md:ml-[6.4rem] lg:ml-0' : ''} lg:ml-[1.4rem] md:ml-[1.4rem]`}>
+                    <IoSearchOutline className='text-xl' />
+                  </div>
+                  <div className={`${isMenuOpen ? 'md:ml-[9rem] lg:ml-0' : ''} lg:ml-[29rem] md:ml-[12.5rem]`}>
+                    <IoCloseOutline className='text-2xl cursor-pointer' onClick={handleClearSearch} />
+                  </div>
+                </div>
+              </>
             )
           }
 
@@ -95,7 +107,7 @@ const SearchBar = ({ showSearch, setShowSearch }) => {
             onBlur={handleInputBlur}
           />
           <button className={searchButtonStyles} onClick={handleSearchButtonClick}>
-            <IoSearchOutline className='md:w-10 md:h-5' />
+            <IoSearchOutline />
           </button>
         </div>
 
