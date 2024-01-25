a2424afeec93f567175e034a2baf3c7de15408ed
Onkar Karale <onkarkarale4@gmail.com>
2024-01-25 23:13:09 +0530

    Add autoplay to the watchPage

--- src/HomePageContainer/WatchPage.jsx
+++ src/HomePageContainer/WatchPage.jsx
@@ -13,7 +13,7 @@ const WatchPage = () => {
     <div className='md:mx-[1.85rem] md:mt-14 max-sm:mt-[5rem] max-sm:mx-auto'>
       <iframe
         className='rounded-lg max-sm:w-[95vw] max-sm:h-[28vh] md:w-[92.5vw] md:h-[40vh] lg:w-[64.3vw] lg:h-[75vh] object-cover'
-        src={videoSrc}
+        src={videoSrc + "?autoplay=1&mute=0"}
         title='YouTube video player'
         frameBorder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
