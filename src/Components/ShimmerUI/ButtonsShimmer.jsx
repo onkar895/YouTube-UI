e20d403cff19d92590b41bbc589123545c02837a
Onkar Karale <onkarkarale4@gmail.com>
2024-01-31 14:39:51 +0530

    Updated ButtonShimmer

--- src/Components/ShimmerUI/ButtonsShimmer.jsx
+++ src/Components/ShimmerUI/ButtonsShimmer.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const ButtonsShimmer = () => {
   return (
     <div className='flex lg:w-[90.2vw] md:w-[80vw] max-sm:w-[94vw] md:h-8 gap-4 max-sm:h-8 animate-pulse'>
-      {Array(20).fill('').map((_, index) => (
-        <div key={index} className='px-8 rounded-lg bg-gray-300'>
+      {Array(12).fill('').map((_, index) => (
+        <div key={index} className='px-[2.55rem] max-sm:px-[2.03rem] rounded-lg bg-gray-300'>
           <div></div>
         </div>
       ))}
