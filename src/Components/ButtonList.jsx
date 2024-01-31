fd9563ad65db2bdd734209dbf86fc772158e5f7d
Onkar Karale <onkarkarale4@gmail.com>
2024-01-31 17:27:44 +0530

    Updated ButtonList

--- src/Components/ButtonList.jsx
+++ src/Components/ButtonList.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useRef } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { ButtonNames } from '../utils/constants';
 import { BiSolidChevronLeftCircle, BiSolidChevronRightCircle } from "react-icons/bi";
 import ButtonsShimmer from './ShimmerUI/ButtonsShimmer';
 
 const ButtonList = () => {
 
-  const [loading, setLoading] = useState(true);
   const listRef = useRef();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
   const [slideNumber, setSlideNumber] = useState(0);
+  const [selectedButton, setSelectedButton] = useState("All"); // State to track selected button
 
   useEffect(() => {
     // Simulate loading data
@@ -30,6 +32,16 @@ const ButtonList = () => {
     } else if (direction === "next") {
       setSlideNumber(slideNumber + 1);
       box.scrollLeft += slideWidth;
+    }
+  };
+
+  const handleButtonClick = (ButtonName) => {
+    const query = ButtonName.replace(" ", "+");
+    setSelectedButton(ButtonName); // Update selected button
+    if (query === "All") {
+      navigate("/");
+    } else {
+      navigate(`/searchresults?search_query=${query}`);
     }
   };
 
@@ -60,17 +72,17 @@ const ButtonList = () => {
               <ButtonsShimmer />
             ) : (
               ButtonNames.map((name, index) => (
-                <li key={index} className={`bg-gray-100 hover:bg-gray-900 hover:text-white hover:transition duration-500 px-[12px] py-[6px] rounded-lg ${name === 'All' ? 'bg-gray-900 text-white' : ''}`}>
-                  <NavLink to={`/${name.toLowerCase()}`}>
+                <li key={index} className={`bg-gray-100 hover:bg-gray-900 hover:text-white hover:transition duration-500 px-[12px] py-[6px] rounded-lg ${selectedButton === name ? 'bg-gray-900 text-white' : ''}`}>
+                  <button onClick={() => handleButtonClick(name)}>
                     <span>{name}</span>
-                  </NavLink>
+                  </button>
                 </li>
               ))
             )
           }
         </ul>
       </div>
-    </div>
+    </div >
   );
 };
 
