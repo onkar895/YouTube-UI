ecedb99b13fb8a851f6422c15c85add41bf2638a
Onkar Karale <onkarkarale4@gmail.com>
2024-02-07 03:23:02 +0530

    Created RelatedVideoPage and Updated WatchPage

--- src/HomePageContainer/WatchPage.jsx
+++ src/HomePageContainer/WatchPage.jsx
@@ -1,17 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useMemo } from 'react';
 import ChannelData from './WatchPageData/ChannelData';
 import { useSearchParams } from 'react-router-dom'
 import CommentsData from './WatchPageData/CommentsData';
 import LiveChat from './WatchPageData/LiveChat';
+import RelatedVideoPage from './WatchPageData/RelatedVideoPage';
 
 const WatchPage = () => {
 
   // useSearchParams is used for accessing and manipulating URL parameters.
   const [searchParams] = useSearchParams();
   const videoId = searchParams.get('v');
-  const videoSrc = `https://www.youtube.com/embed/${videoId}`;
+  const videoSrc = useMemo(() => `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`, [videoId]);
 
   return (
     <div className='md:flex md:gap-8 md:mt-[4rem] max-sm:mt-[4.8rem]'>
@@ -19,7 +20,7 @@ const WatchPage = () => {
         <div>
           <iframe
             className='rounded-2xl max-sm:w-[95vw] max-sm:h-[28vh] md:w-[92.5vw] md:h-[40vh] lg:w-[64.5vw] lg:h-[81vh] object-cover'
-            src={`${videoSrc}?autoplay=1&mute=0`}
+            src={videoSrc}
             title='YouTube video player'
             frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
@@ -38,6 +39,7 @@ const WatchPage = () => {
       </div>
       <div className='hidden md:block'>
         <LiveChat />
+        <RelatedVideoPage />
       </div>
     </div>
 
---
+++ src/HomePageContainer/WatchPageData/RelatedVideoPage.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const RelatedVideoPage = () => {
+  return (
+    <div>
+      RelatedVideoPage
+    </div>
+  )
+}
+
+export default RelatedVideoPage
